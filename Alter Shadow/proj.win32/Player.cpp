bd656d232ff0ba8e0f3d8bdaadcb5c98543ea8a3
Josh Sankarlal <joshua.sankarlal@uoit.net>
2018-02-06 16:47:44 -0500

    Pushing player.h and player.cpp

--- Alter Shadow/proj.win32/Player.cpp
+++ Alter Shadow/proj.win32/Player.cpp
@@ -2,11 +2,26 @@
 
 
 
-Player::Player()
+Player::Player(string _name)
 {
+	playerIdentifier = _name;
+	MaxHP = 200;
+	HP = 200;
+	lightDamage = 5;
+	heavyDamage = 7;
+	heavyDamageCharged = 12;
+	throwAttack = 5;
+	crossAttack = 15;
+
 }
 
 
 Player::~Player()
 {
+
+
+
+
+
+
 }
