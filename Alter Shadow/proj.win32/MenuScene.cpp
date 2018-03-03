dfb06d9a9de554bc94357cfe0b6a683712b821d3
Robert Andersen <robert.andersen@uoit.net>
2018-03-03 14:38:45 -0500

    New scene MainScene made, program not broken. Next on list is to make scene into menu then make it default scene upon startup.

---
+++ Alter Shadow/proj.win32/MenuScene.cpp
@@ -0,0 +1,82 @@
+#pragma once 
+#include "MenuScene.h"
+
+USING_NS_CC;
+using namespace std;
+Scene* MenuScene::createScene()
+{
+	auto scenepb = MenuScene::createWithPhysics();
+	//	scenepb->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
+
+	auto world = MenuScene::create();
+	scenepb->addChild(world);
+	//setScene(world);
+	scenepb->getPhysicsWorld()->setGravity(Vec2(0, -1500));
+	return scenepb;
+}
+
+// Print useful error message instead of segfaulting when files are not there.
+static void problemLoading(const char* filename)
+{
+	printf("Error while loading: %s\n", filename);
+	printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in MenuSceneScene.cpp\n");
+}
+
+// on "init" you need to initialize your instance
+bool MenuScene::init()
+{
+
+	//////////////////////////////
+	// 1. super init first
+	if (!Scene::init())
+		return false;
+
+	director = Director::getInstance();
+	auto visibleSize = director->getVisibleSize();
+	Vec2 origin = director->getVisibleOrigin();
+
+	//Background
+	//background->setScaleX(visibleSize.width / background->getContentSize().width);
+	//background->setScaleY(visibleSize.height / background->getContentSize().height);
+	//background->setPosition(visibleSize.width / 2, visibleSize.height / 2);
+	//addChild(background, -1);
+
+
+	//call update
+	this->scheduleUpdate();
+
+	//Background Audio
+	audio->setAudio("Audio/Battle_Time_V3.mp3");
+	audio->play(true);
+
+	return true;
+}
+
+//Updates movement per frame
+void MenuScene::update(float dt)
+{
+	
+}
+
+void MenuScene::menuCloseCallback(Ref* pSender)
+{
+	//Close the cocos2d-x game scene and quit the application
+	Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+	exit(0);
+#endif
+
+	/*To navigate back to native iOS screen(if present) without quitting the application,
+	do not use Director::getInstance()->end() and exit(0) as given above, instead trigger
+	a custom event created in RootViewController.mm as below*/
+
+	//EventCustom customEndEvent("game_scene_close_event");
+	//_eventDispatcher->dispatchEvent(&customEndEvent);
+
+}
+
+void MenuScene::DrawWorld()
+{
+
+}
