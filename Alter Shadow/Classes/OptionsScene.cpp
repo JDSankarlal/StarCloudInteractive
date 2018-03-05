da6abd3d3d764819d7ea3b6f747167bbf19f1901
Robert Andersen <robert.andersen@uoit.net>
2018-03-05 16:41:12 -0500

    Options Menu, almost works.

--- Alter Shadow/Classes/OptionsScene.cpp
+++ Alter Shadow/Classes/OptionsScene.cpp
@@ -2,13 +2,12 @@
 #include "MenuScene.h"
 #include "HelloWorld.h"
 #include "OptionsScene.h"
-
 USING_NS_CC;
 using namespace std;
 using namespace Input;
-Scene* MenuScene::createScene()
+Scene* OptionsScene::createScene()
 {
-	return  MenuScene::create();
+	return  OptionsScene::create();
 }
 
 // Print useful error message instead of segfaulting when files are not there.
@@ -19,7 +18,7 @@ static void problemLoading(const char* filename)
 }
 
 // on "init" you need to initialize your instance
-bool MenuScene::init()
+bool OptionsScene::init()
 {
 
 	//////////////////////////////
@@ -37,23 +36,24 @@ bool MenuScene::init()
 	background->setPosition(visibleSize.width / 2, visibleSize.height / 2);
 	addChild(background, -1);
 
-	playBtn = Sprite::create("pics/playBtn.png");
-	playBtn->setPosition(visibleSize.width / 2, visibleSize.height / 2);
-	playBtn->setScale(1);
-	playBtn->setAnchorPoint(Vec2(0.5f, 0.5f));
-	this->addChild(playBtn, 2);
+	optionsHeader = Sprite::create("pics/OptionsBtn.png");
+	optionsHeader->setPosition(visibleSize.width / 2, (visibleSize.height / 2) + 200);
+	optionsHeader->setScale(2);
+	optionsHeader->setAnchorPoint(Vec2(0.5f, 0.5f));
+	this->addChild(optionsHeader, 2);
 
-	optionsBtn = Sprite::create("pics/OptionsBtn.png");
-	optionsBtn->setPosition(visibleSize.width / 2, (visibleSize.height / 2) - 125);
-	optionsBtn->setScale(1);
-	optionsBtn->setAnchorPoint(Vec2(0.5f, 0.5f));
-	this->addChild(optionsBtn, 2);
+	//musicBtn = Sprite::create("pics/musicBtn.png");
+	//musicBtn->setPosition(visibleSize.width / 2, (visibleSize.height / 2) - 125);
+	//musicBtn->setScale(1);
+	//musicBtn->setAnchorPoint(Vec2(0.5f, 0.5f));
+	//this->addChild(musicBtn, 2);
 
-	quitBtn = Sprite::create("pics/quitbtn.png");
-	quitBtn->setPosition(visibleSize.width / 2, (visibleSize.height / 2) - 250);
-	quitBtn->setScale(1);
-	quitBtn->setAnchorPoint(Vec2(0.5f, 0.5f));
-	this->addChild(quitBtn, 2);
+	backBtn = Sprite::create("pics/backBtn.png");
+	backBtn->setPosition(visibleSize.width / 2, (visibleSize.height / 2) - 250);
+	backBtn->setScale(1);
+	backBtn->setAnchorPoint(Vec2(0.5f, 0.5f));
+	this->addChild(backBtn, 2);
+
 
 	//call update
 	this->scheduleUpdate();
@@ -68,7 +68,7 @@ bool MenuScene::init()
 }
 
 //Updates movement per frame
-void MenuScene::update(float index)
+void OptionsScene::update(float index)
 {
 	static XBoxInput controllers;
 	controllers.DownloadPackets(4);
@@ -76,88 +76,47 @@ void MenuScene::update(float index)
 
 	if (controllers.GetConnected(index))
 	{
-	
 		Stick moveD, moveU;
-		static int count,til=20;
+		static int count, til = 20;
 		controllers.GetSticks(index, moveD, moveU);
-		if (playBtnActive)
+		if (backBtnActive)
 		{
-			playBtn->setScale(1.3f);
-	
+			backBtn->setScale(1.3f);
+
+				if (controllers.ButtonPress(index, A) == 0)
+				{
+					count = til;
+				}
+			
+		if (count++ > til) {
+			count = 0;
 			if (controllers.ButtonPress(index, A))
 			{
-				Director::getInstance()->replaceScene(HelloWorld::createScene());
+				//Director::getInstance()->replaceScene(MenuScene::createScene());
 			}
+		}
 			if (moveD.yAxis == 0)
 			{
 				count = til;
 			}
 			if (count++>til) {
 				count = 0;
-				if (moveD.yAxis < 0)
-				{
-					optionsBtnActive = true;
-					playBtn->setScale(1);
-					playBtnActive = false;
-				}
-			}
-		}
-		else if (optionsBtnActive)
-		{
-			optionsBtn->setScale(1.3f);
-	
-			if (controllers.ButtonPress(index, A))
-			{
-				Director::getInstance()->replaceScene(OptionsScene::createScene());
-			}
-			if (moveD.yAxis == 0)
-			{
-				count = til;
-			}
-			if (count++ > til) {
-				count = 0;
-				if (moveD.yAxis < 0)
-				{
-					quitBtnActive = true;
-					optionsBtn->setScale(1);
-					optionsBtnActive = false;
-				}
 				if (moveD.yAxis > 0)
 				{
-					playBtnActive = true;
-					optionsBtn->setScale(1);
-					optionsBtnActive = false;
+					musicBtnActive = true;
+					backBtn->setScale(1);
+					backBtnActive = false;
 				}
 			}
 		}
-		else if (quitBtnActive)
-		{
-			quitBtn->setScale(1.3f);
-	
-			if (controllers.ButtonPress(index, A))
-			{
-				//System.exit(0);
-			}
-			if (moveD.yAxis == 0)
-			{
-				count = til;
-			}
-			if (count++ > til) {
-				count = 0;
-				if (moveD.yAxis > 0)
-				{
-					optionsBtnActive = true;
-					quitBtn->setScale(1);
-					quitBtnActive = false;
-				}
-			}
-			
-		}
-	
+		
+
+		
+
 	}
 }
 
-void MenuScene::menuCloseCallback(Ref* pSender)
+void OptionsScene::menuCloseCallback(Ref* pSender)
 {
 	//Close the cocos2d-x game scene and quit the application
 	Director::getInstance()->end();
@@ -175,7 +134,7 @@ void MenuScene::menuCloseCallback(Ref* pSender)
 
 }
 
-void MenuScene::DrawWorld()
+void OptionsScene::DrawWorld()
 {
 
 }
